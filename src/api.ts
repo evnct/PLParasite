3d27a8a65563bd10d78e32e9f938851f2df854c8
LiuDi <404637575@qq.com>
2023-05-18 10:49:58 +0800

    add automatically update season

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,23 @@
-import axios from 'axios';
-import { ILeague } from './models/League';
+import axios from "axios"
+import { ILeague } from "./models/League"
 
 export async function fetchLeagueData(): Promise<ILeague | null> {
-    try {
-        const response = await axios.get<ILeague>(`https://api-football-standings.azharimm.dev/leagues/eng.1/standings?season=2022&sort=asc`);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        return null;
+  try {
+    const currentDate = new Date()
+    const currentMonth = currentDate.getMonth() + 1
+    const currentYear = currentDate.getFullYear()
+    let seasonYear
+    if (currentMonth >= 8) {
+      seasonYear = currentYear
+    } else {
+      seasonYear = currentYear - 1
     }
+    const response = await axios.get<ILeague>(
+      `https://api-football-standings.azharimm.dev/leagues/eng.1/standings?season=${seasonYear}&sort=asc`
+    )
+    return response.data
+  } catch (error) {
+    console.error(error)
+    return null
+  }
 }
