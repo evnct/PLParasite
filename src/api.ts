ccc35321467ac7b629009a29beabb4f29ea00f9e
LiuDi <404637575@qq.com>
2023-05-19 17:38:18 +0800

    update date by month

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,56 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { ILeague } from "./models/League"
 
 export async function fetchLeagueData(): Promise<ILeague | null> {
-  try {
-    const currentDate = new Date()
-    const currentMonth = currentDate.getMonth() + 1
-    const currentYear = currentDate.getFullYear()
-    let seasonYear
-    if (currentMonth >= 8) {
-      seasonYear = currentYear
-    } else {
-      seasonYear = currentYear - 1
+  const currentDate = new Date()
+  const currentMonth = currentDate.getMonth() + 1
+
+  const currentYear = currentDate.getFullYear()
+  if (currentMonth < 8) {
+    try {
+      const response = await axios.get<ILeague>(
+        `https://api-football-standings.azharimm.dev/leagues/eng.1/standings?season=${
+          currentYear - 1
+        }&sort=asc`
+      )
+      return response.data
+    } catch (error) {
+      console.error(error)
     }
-    const response = await axios.get<ILeague>(
-      `https://api-football-standings.azharimm.dev/leagues/eng.1/standings?season=${seasonYear}&sort=asc`
-    )
-    return response.data
-  } catch (error) {
-    console.error(error)
-    return null
+  } else if (currentMonth >= 10) {
+    try {
+      const response = await axios.get<ILeague>(
+        `https://api-football-standings.azharimm.dev/leagues/eng.1/standings?season=${currentYear}&sort=asc`
+      )
+      return response.data
+    } catch (error) {
+      console.error(error)
+    }
+  } else {
+    try {
+      const response = await axios.get<ILeague>(
+        `https://api-football-standings.azharimm.dev/leagues/eng.1/standings?season=${currentYear}&sort=asc`
+      )
+      return response.data
+    } catch (error: any) {
+      if (
+        error?.response?.data?.data?.includes(
+          "Standings data not available for this"
+        )
+      ) {
+        try {
+          const response = await axios.get<ILeague>(
+            `https://api-football-standings.azharimm.dev/leagues/eng.1/standings?season=${
+              currentYear - 1
+            }&sort=asc`
+          )
+          return response.data
+        } catch (error) {
+          console.error(error)
+        }
+      }
+      console.error(error)
+    }
   }
+  return null
 }
